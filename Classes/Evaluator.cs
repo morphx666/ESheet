fb274663201cbdbaaef640c389ddd032c4b993b3
Xavier Flix <morphx666@users.noreply.github.com>
2025-09-07 19:43:16 -0400

    Keeping the c# compiler's linter happy

--- Classes/Evaluator.cs
+++ Classes/Evaluator.cs
@@ -27,61 +27,66 @@ internal class Evaluator {
     public Dictionary<int, string> Strings { get => strings; }
 
     class MathFuncDef {
-        public string Name { get; set; } = "";
-        public int MinParamCount { get; set; } = 0; // -1 for variable number of parameters
-        public MathFunc? Func { get; set; } = null;
-        public string Description { get; set; } = "";
+        public string Name { get; init; }
+        public int MinParamCount { get; init; }
+        public MathFunc Function { get; init; }
+        public string Description { get; init; }
 
-        public MathFuncDef() { }
+        public MathFuncDef(string name, int minParamCount, MathFunc function, string description = "") {
+            Name = name;
+            MinParamCount = minParamCount;
+            Function = function;
+            Description = description;
+        }
     }
 
-    delegate object MathFunc(FunctionArgs x);
-    private static List<MathFuncDef> functions = null!;
-    public static string[] FunctionNames { get => functions.Select(f => f.Name).ToArray(); }
+    private delegate object MathFunc(FunctionArgs x);
+    private readonly static List<MathFuncDef> functions = [];
+    public static string[] FunctionNames {  get => [.. functions.Select(f => f.Name)]; }
 
     public Evaluator() {
-        if(functions != null) return;
-        functions = [
-            new() { Name = "ABS",   MinParamCount = 1, Func = args => Math.Abs(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the absolute value of parameter" },
-            new() { Name = "AVG",   MinParamCount = 2, Func = args => args.Parameters.Average(p => Convert.ToDouble(p.Evaluate())), Description = "Calculates the average (arithmetic mean) of parameters" },
-            new() { Name = "COS",   MinParamCount = 1, Func = args => Math.Cos(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the cosine of parameter (in radians)" },
-            new() { Name = "EXP",   MinParamCount = 1, Func = args => Math.Exp(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates e raised to the power of parameter" },
-            new() { Name = "IIF",   MinParamCount = 3, Func = args => Convert.ToBoolean(args.Parameters[0].Evaluate()) ? args.Parameters[1].Evaluate() : args.Parameters[2].Evaluate(), Description = "If the first parameter is true, returns the second parameter, otherwise the third parameter" },
-            new() { Name = "INT",   MinParamCount = 1, Func = args => Math.Floor(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the integer part of parameter" },
-            new() { Name = "LN",    MinParamCount = 1, Func = args => Math.Log(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the natural logarithm of parameter" },
-            new() { Name = "LOG10", MinParamCount = 1, Func = args => Math.Log10(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the base-10 logarithm of parameter" },
-            new() { Name = "LOG2",  MinParamCount = 1, Func = args => Math.Log2(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the base-2 logarithm of parameter" },
-            new() { Name = "MAX",   MinParamCount = 2, Func = args => args.Parameters.Max(p => Convert.ToDouble(p.Evaluate())), Description = "Calculates the maximum value of parameters" },
-            new() { Name = "MIN",   MinParamCount = 2, Func = args => args.Parameters.Min(p => Convert.ToDouble(p.Evaluate())), Description = "Calculates the minimum value of parameters" },
-            new() { Name = "MOD",   MinParamCount = 2, Func = args => Convert.ToDouble(args.Parameters[0].Evaluate()) % Convert.ToDouble(args.Parameters[1].Evaluate()), Description = "Calculates the modulus of two parameters" },
-            new() { Name = "POW",   MinParamCount = 2, Func = args => Math.Pow(Convert.ToDouble(args.Parameters[0].Evaluate()), Convert.ToDouble(args.Parameters[1].Evaluate())), Description = "Calculates the power of the first parameter raised to the second parameter" },
-            new() { Name = "RAND",  MinParamCount = 0, Func = args => new Random().NextDouble(), Description = "Generates a random number between 0 and 1" },
-            new() { Name = "SIN",   MinParamCount = 1, Func = args => Math.Sin(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the sine of parameter (in radians)" },
-            new() { Name = "SQRT",  MinParamCount = 1, Func = args => Math.Sqrt(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the square root of parameter" },
-            new() { Name = "STD",   MinParamCount = 2, Func = args => {
+        if(functions.Count > 0) return;
+        functions.AddRange([
+            new("ABS",      1, args => Math.Abs(Convert.ToDouble(args.Parameters[0].Evaluate())),       "Calculates the absolute value of parameter"),
+            new("AVG",      2, args => args.Parameters.Average(p => Convert.ToDouble(p.Evaluate())),    "Calculates the average (arithmetic mean) of parameters"),
+            new("COS",      1, args => Math.Cos(Convert.ToDouble(args.Parameters[0].Evaluate())),       "Calculates the cosine of parameter (in radians)"),
+            new("EXP",      1, args => Math.Exp(Convert.ToDouble(args.Parameters[0].Evaluate())),       "Calculates e raised to the power of parameter"),
+            new("IIF",      3, args => Convert.ToBoolean(args.Parameters[0].Evaluate())? args.Parameters[1].Evaluate() : args.Parameters[2].Evaluate(), "If the first parameter is true, returns the second parameter, otherwise the third parameter"),
+            new("INT",      1, args => Math.Floor(Convert.ToDouble(args.Parameters[0].Evaluate())),     "Calculates the integer part of parameter"),
+            new("LN",       1, args => Math.Log(Convert.ToDouble(args.Parameters[0].Evaluate())),       "Calculates the natural logarithm of parameter"),
+            new("LOG10",    1, args => Math.Log10(Convert.ToDouble(args.Parameters[0].Evaluate())),     "Calculates the base-10 logarithm of parameter"),
+            new("LOG2",     1, args => Math.Log2(Convert.ToDouble(args.Parameters[0].Evaluate())),      "Calculates the base-2 logarithm of parameter"),
+            new("MAX",      2, args => args.Parameters.Max(p => Convert.ToDouble(p.Evaluate())),        "Calculates the maximum value of parameters"),
+            new("MIN",      2, args => args.Parameters.Min(p => Convert.ToDouble(p.Evaluate())),        "Calculates the minimum value of parameters"),
+            new("MOD",      2, args => Convert.ToDouble(args.Parameters[0].Evaluate()) % Convert.ToDouble(args.Parameters[1].Evaluate()), "Calculates the modulus of two parameters"),
+            new("POW",      2, args => Math.Pow(Convert.ToDouble(args.Parameters[0].Evaluate()), Convert.ToDouble(args.Parameters[1].Evaluate())), "Calculates the power of the first parameter raised to the second parameter"),
+            new("RAND",     0, args => new Random().NextDouble(),                                       "Generates a random number between 0 and 1"),
+            new("SIN",      1, args => Math.Sin(Convert.ToDouble(args.Parameters[0].Evaluate())),       "Calculates the sine of parameter (in radians)"),
+            new("SQRT",     1, args => Math.Sqrt(Convert.ToDouble(args.Parameters[0].Evaluate())),      "Calculates the square root of parameter"),
+            new("STD",      2, args => {
                     double mean = args.Parameters.Average(p => Convert.ToDouble(p.Evaluate()));
                     double sumOfSquares = args.Parameters.Select(v => Math.Pow(Convert.ToDouble(v.Evaluate()) - mean, 2)).Sum();
                     return Math.Sqrt(sumOfSquares / args.Parameters.Length);
                 },
-                Description = "Calculates the standard deviation of parameters"
-            },
-            new() { Name = "STDS",  MinParamCount = 2, Func = args => {
+                "Calculates the standard deviation of parameters"
+            ),
+            new("STDS",     2, args => {
                     double mean = args.Parameters.Average(p => Convert.ToDouble(p.Evaluate()));
                     double sumOfSquares = args.Parameters.Select(v => Math.Pow(Convert.ToDouble(v.Evaluate()) - mean, 2)).Sum();
                     return Math.Sqrt(sumOfSquares / (args.Parameters.Length - 1));
                 },
-                Description = "Calculates the sample standard deviation of parameters"
-            },
-            new() { Name = "STR",   MinParamCount = 1, Func = args => strings[Convert.ToInt32(args.Parameters[0].Evaluate())], Description = "Retrieves the string at the specified index" },
-            new() { Name = "SUM",   MinParamCount = 2, Func = args => args.Parameters.Sum(p => Convert.ToDouble(p.Evaluate())), Description = "Calculates the sum of parameters" },
-            new() { Name = "TAN",   MinParamCount = 1, Func = args => Math.Tan(Convert.ToDouble(args.Parameters[0].Evaluate())), Description = "Calculates the tangent of parameter (in radians)" },
-            new() { Name = "TODEG", MinParamCount = 1, Func = args => Convert.ToDouble(args.Parameters[0].Evaluate()) / ToRad, Description = "Converts radians to degrees" },
-            new() { Name = "TORAD", MinParamCount = 1, Func = args => Convert.ToDouble(args.Parameters[0].Evaluate()) * ToRad, Description = "Converts degrees to radians" }
-        ];
+                "Calculates the sample standard deviation of parameters"
+            ),
+            new("STR",      1, args => strings[Convert.ToInt32(args.Parameters[0].Evaluate())],         "Retrieves the string at the specified index"),
+            new("SUM",      2, args => args.Parameters.Sum(p => Convert.ToDouble(p.Evaluate())),        "Calculates the sum of parameters"),
+            new("TAN",      1, args => Math.Tan(Convert.ToDouble(args.Parameters[0].Evaluate())),       "Calculates the tangent of parameter (in radians)"),
+            new("TODEG",    1, args => Convert.ToDouble(args.Parameters[0].Evaluate()) / ToRad,         "Converts radians to degrees"),
+            new("TORAD",    1, args => Convert.ToDouble(args.Parameters[0].Evaluate()) * ToRad,         "Converts degrees to radians")
+        ]);
     }
 
     public string Formula {
-        get { return formula; }
+        get => formula;
         set {
             formula = value;
             if(formula == "") formula = "0";
@@ -91,7 +96,7 @@ internal class Evaluator {
                 var func = functions.FirstOrDefault(f => f.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
                 if(func != null) {
                     if(args.Parameters.Length < func.MinParamCount) throw new ArgumentException($"{func.Name} function requires at least {func.MinParamCount} parameter{(func.MinParamCount == 1 ? "" : "s")}");
-                    args.Result = func.Func(args);
+                    args.Result = func.Function(args);
                     return;
                 }
             };
